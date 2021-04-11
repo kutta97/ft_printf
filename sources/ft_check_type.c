a40710fcf337eb48a2589bc3504e8978a1e6a75b
kutta97 <yhjin04170@gmail.com>
2021-04-12 02:52:29 +0900

    add ft_check_type

--- sources/ft_check_type.c
+++ sources/ft_check_type.c
@@ -6,7 +6,7 @@
 /*   By: hyyang <hyyang@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/12 01:37:18 by hyyang            #+#    #+#             */
-/*   Updated: 2021/04/12 01:47:26 by hyyang           ###   ########.fr       */
+/*   Updated: 2021/04/12 02:46:14 by hyyang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,20 @@
 
 void	ft_check_type(va_list ap, char *format, int *i, t_conversions *conv)
 {
-
+	if ((format[*i] == 'c') ? (*i)++, (conv->type = c) : 0)
+		return ;
+	if ((format[*i] == 's') ? (*i)++, (conv->type = s) : 0)
+		return ;
+	if ((format[*i] == 'p') ? (*i)++, (conv->type = p) : 0)
+		return ;
+	if ((format[*i] == 'd') ? (*i)++, (conv->type = d) : 0)
+		return ;
+	if ((format[*i] == 'i') ? (*i)++, (conv->type = i) : 0)
+		return ;
+	if ((format[*i] == 'u') ? (*i)++, (conv->type = u) : 0)
+		return ;
+	if ((format[*i] == 'x') ? (*i)++, (conv->type = x) : 0)
+		return ;
+	if ((format[*i] == 'X') ? (*i)++, (conv->type = X) : 0)
+		return ;
 }
