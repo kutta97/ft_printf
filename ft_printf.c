8d9c7c62504534142f9b722f7dcfecf71f7da49f
kutta97 <yhjin04170@gmail.com>
2021-04-11 21:52:35 +0900

    modify ft_putchar to return the count of written characters

--- ft_printf.c
+++ ft_printf.c
@@ -6,17 +6,25 @@
 /*   By: hyyang <hyyang@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/11 21:14:58 by hyyang            #+#    #+#             */
-/*   Updated: 2021/04/11 21:22:37 by hyyang           ###   ########.fr       */
+/*   Updated: 2021/04/11 21:46:39 by hyyang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "inlcudes/ft_printf.h"
 
+int	ft_parse_format(va_list ap, char *format)
+{
+	int	i;
+	int ncp;
+}
+
 int	ft_printf(const char *format)
 {
 	va_list	ap;
-	int		num_of_chrs;
+	int		ncp;
 
 	va_start(ap, format);
-	return (num_of_chrs)
+	ncp = ft_parse_format(ap, (char *)format);
+	va_end(ap);
+	return (ncp)
 }
