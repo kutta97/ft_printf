8d790f4d88739d16b1ed7e9d7af84949c290dd6b
kutta97 <yhjin04170@gmail.com>
2021-04-11 21:23:37 +0900

    add ft_printf.c

---
+++ ft_printf.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hyyang <hyyang@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/11 21:14:58 by hyyang            #+#    #+#             */
+/*   Updated: 2021/04/11 21:22:37 by hyyang           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "inlcudes/ft_printf.h"
+
+int	ft_printf(const char *format)
+{
+	va_list	ap;
+	int		num_of_chrs;
+
+	va_start(ap, format);
+	return (num_of_chrs)
+}
