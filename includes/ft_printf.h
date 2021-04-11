38abf2bb3bec21510f63368d2271a8df629ceaef
kutta97 <yhjin04170@gmail.com>
2021-04-12 03:32:08 +0900

    add print functions

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: hyyang <hyyang@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/11 21:08:43 by hyyang            #+#    #+#             */
-/*   Updated: 2021/04/12 02:54:31 by hyyang           ###   ########.fr       */
+/*   Updated: 2021/04/12 03:29:10 by hyyang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 # include "../libft/libft.h"
 # include <stdarg.h>
 
-# define TYPE "csdiupxX"
+# define TYPE "csdiupxX%"
 
 typedef	struct	s_flags
 {
@@ -35,9 +35,16 @@ typedef struct	s_conversions
 int				ft_printf(const char *format, ...);
 int				ft_putchar(int c);
 void			ft_init_conversions(t_conversions *conv);
-void			ft_check_flags(va_list ap, char *format, int *i, t_conversions *conv);
-void			ft_check_width(va_list ap, char *format, int *i, t_conversions *conv);
-void			ft_check_precision(va_list ap, char *format, int *i, t_conversions *conv);
-void			ft_check_type(va_list ap, char *format, int *i, t_conversions *conv);
+void			ft_check_flags(va_list ap, char *format, int *i,
+													t_conversions *conv);
+void			ft_check_width(va_list ap, char *format, int *i,
+													t_conversions *conv);
+void			ft_check_precision(va_list ap, char *format, int *i,
+													t_conversions *conv);
+void			ft_check_type(va_list ap, char *format, int *i,
+													t_conversions *conv);
+int				ft_print_char(int c, t_conversions *conv);
+int				ft_print_string(char *str, t_conversions *conv);
+int				ft_print_nbr(unsigned long long nbr, t_conversions *conv);
 
 #endif
