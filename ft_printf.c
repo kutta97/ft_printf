c82df417255db009d61766a21f9e1e053a73996c
kutta97 <yhjin04170@gmail.com>
2021-04-12 03:11:23 +0900

    modify ft_analyze_conversions

--- ft_printf.c
+++ ft_printf.c
@@ -6,27 +6,26 @@
 /*   By: hyyang <hyyang@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/11 21:14:58 by hyyang            #+#    #+#             */
-/*   Updated: 2021/04/12 01:46:50 by hyyang           ###   ########.fr       */
+/*   Updated: 2021/04/12 03:05:49 by hyyang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes/ft_printf.h"
 
-int	ft_print_conversions(t_conversions *conv)
+int		ft_print_conversions(va_list ap, t_conversions *conv)
 {
-
+	int	ncp;
 }
 
-int	ft_analyze_conversions(va_list ap, char *format, int i, t_conversions *conv)
+void	ft_analyze_conversions(va_list ap, char *format, int *i, t_conversions *conv)
 {
-	ft_check_flags(ap, format, &i, conv);
-	ft_check_width(ap, format, &i, conv);
-	ft_check_precision(ap, format, &i, conv);
-	ft_check_type(ap, format, &i, conv);
-	return (i);
+	ft_check_flags(ap, format, i, conv);
+	ft_check_width(ap, format, i, conv);
+	ft_check_precision(ap, format, i, conv);
+	ft_check_type(ap, format, i, conv);
 }
 
-int	ft_parse_format(va_list ap, char *format)
+int		ft_parse_format(va_list ap, char *format)
 {
 	int				i;
 	int				ncp;
@@ -41,8 +40,8 @@ int	ft_parse_format(va_list ap, char *format)
 		if (format[i++] == '%')
 		{
 			ft_init_conversions(conv);
-			ft_analyze_conversions(ap, format, i, conv);
-			ft_print_conversions(conv);
+			ft_analyze_conversions(ap, format, &i, conv);
+			ncp += ft_print_conversions(ap, conv);
 		}
 		ncp += ft_putchar(format[i++]);
 	}
@@ -50,7 +49,7 @@ int	ft_parse_format(va_list ap, char *format)
 	return (ncp);
 }
 
-int	ft_printf(const char *format, ...)
+int		ft_printf(const char *format, ...)
 {
 	va_list	ap;
 	int		num_of_chrs_printed;
