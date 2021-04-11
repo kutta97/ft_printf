88e47d4111a6e699812bb08ea53a6ef90992b494
kutta97 <yhjin04170@gmail.com>
2021-04-11 22:24:17 +0900

    ft_printf.c can print plain characters without formats

--- ft_printf.c
+++ ft_printf.c
@@ -6,25 +6,37 @@
 /*   By: hyyang <hyyang@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/11 21:14:58 by hyyang            #+#    #+#             */
-/*   Updated: 2021/04/11 21:46:39 by hyyang           ###   ########.fr       */
+/*   Updated: 2021/04/11 22:04:11 by hyyang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "inlcudes/ft_printf.h"
+#include "includes/ft_printf.h"
 
 int	ft_parse_format(va_list ap, char *format)
 {
 	int	i;
 	int ncp;
+
+	i = 0;
+	ncp = 0;
+	while (format[i])
+	{
+		ncp += ft_putchar(format[i++]);
+		if (i < 0)
+		{
+			va_arg(ap, char*);
+		}
+	}
+	return ncp;
 }
 
-int	ft_printf(const char *format)
+int	ft_printf(const char *format, ...)
 {
 	va_list	ap;
-	int		ncp;
+	int		num_of_chrs_printed;
 
 	va_start(ap, format);
-	ncp = ft_parse_format(ap, (char *)format);
+	num_of_chrs_printed = ft_parse_format(ap, (char *)format);
 	va_end(ap);
-	return (ncp)
+	return (num_of_chrs_printed);
 }
