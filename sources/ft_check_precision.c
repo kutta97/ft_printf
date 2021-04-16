82697e9c8aef7a0b1c2f8bdb73c5c2193137ece9
kutta97 <yhjin04170@gmail.com>
2021-04-16 14:55:32 +0900

    modify check_precision

--- sources/ft_check_precision.c
+++ sources/ft_check_precision.c
@@ -6,25 +6,42 @@
 /*   By: hyyang <hyyang@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/12 01:37:07 by hyyang            #+#    #+#             */
-/*   Updated: 2021/04/13 01:07:15 by hyyang           ###   ########.fr       */
+/*   Updated: 2021/04/16 14:55:04 by hyyang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft_printf.h"
+
+int		ft_check_digits(char *format, int *i)
+{
+	int nbr;
+
+	nbr = 0;
+	while (ft_isdigit(format[*i]))
+		nbr = (nbr * 10) + format[(*i)++] - '0';
+	return (nbr);
+}
 
 void	ft_check_precision(va_list ap, char *format, int *i, t_convs *conv)
 {
 	if (format[*i] != '.')
 		return ;
 	(*i)++;
+	conv->precision = 0;
+	if (format[*i] == '-')
+	{
+		conv->flags.minus = 1;
+		while (format[*i] == '-')
+			(*i)++;
+		if (ft_isdigit(format[*i]))
+		{
+			conv->width = ft_check_digits(format, i);
+			return ;
+		}
+	}
 	if (ft_isdigit(format[*i]))
 	{
-		int width;
-
-		width = 0;
-		while (ft_isdigit(format[*i]))
-			width = (width * 10) + format[(*i)++] - '0';
-		conv->precision = width;
+		conv->precision = ft_check_digits(format, i);
 		return ;
 	}
 	if (format[*i] == '*')
