098bf9313d0e0e34090953811672a64f6ca7dde9
kutta97 <yhjin04170@gmail.com>
2021-06-13 16:51:51 +0900

    add initiating conv->wildcard in ft_init_conversions

--- sources/ft_init_conversions.c
+++ sources/ft_init_conversions.c
@@ -6,7 +6,7 @@
 /*   By: hyyang <hyyang@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/12 01:20:11 by hyyang            #+#    #+#             */
-/*   Updated: 2021/04/13 01:07:13 by hyyang           ###   ########.fr       */
+/*   Updated: 2021/06/13 16:27:36 by hyyang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@ void	ft_init_conversions(t_convs *conv)
 	conv->flags.minus = 0;
 	conv->flags.zero = 0;
 	conv->precision = -1;
+	conv->wildcard = 0;
 	conv->width = 0;
 	conv->type = 0;
 }
