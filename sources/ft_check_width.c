d3692757738b90cfc578765ca20f6d03141af45b
kutta97 <yhjin04170@gmail.com>
2021-06-13 18:42:17 +0900

    make ft_check_width to process minus width and space

--- sources/ft_check_width.c
+++ sources/ft_check_width.c
@@ -6,7 +6,7 @@
 /*   By: hyyang <hyyang@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/12 01:36:21 by hyyang            #+#    #+#             */
-/*   Updated: 2021/06/13 15:07:01 by hyyang           ###   ########.fr       */
+/*   Updated: 2021/06/13 17:18:08 by hyyang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ va_list	*ft_check_width(va_list *ap, char *format, int *i, t_convs *conv)
 {
 	int	width;
 
+	if (format[*i] == ' ')
+		ft_putchar(format[(*i)++]);
 	if (ft_isdigit(format[*i]))
 	{
 		width = 0;
@@ -26,7 +28,14 @@ va_list	*ft_check_width(va_list *ap, char *format, int *i, t_convs *conv)
 	}
 	if (format[*i] == '*')
 	{
-		conv->width = va_arg(*ap, int);
+		width = va_arg(*ap, int);
+		if (width < 0)
+		{
+			conv->flags.minus = 1;
+			conv->width = -width;
+		}
+		else
+			conv->width = width;
 		(*i)++;
 		return (ap);
 	}
