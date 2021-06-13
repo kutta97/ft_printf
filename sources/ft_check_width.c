ff242991f097bcff772f348c92a8bcbd7f95c11a
kutta97 <yhjin04170@gmail.com>
2021-06-13 15:12:48 +0900

    make ft_check_width to return ap pointer address

--- sources/ft_check_width.c
+++ sources/ft_check_width.c
@@ -6,13 +6,13 @@
 /*   By: hyyang <hyyang@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/12 01:36:21 by hyyang            #+#    #+#             */
-/*   Updated: 2021/04/16 15:46:24 by hyyang           ###   ########.fr       */
+/*   Updated: 2021/06/13 15:07:01 by hyyang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft_printf.h"
 
-void	ft_check_width(va_list ap, char *format, int *i, t_convs *conv)
+va_list	*ft_check_width(va_list *ap, char *format, int *i, t_convs *conv)
 {
 	int	width;
 
@@ -22,12 +22,13 @@ void	ft_check_width(va_list ap, char *format, int *i, t_convs *conv)
 		while (ft_isdigit(format[*i]))
 			width = (width * 10) + format[(*i)++] - '0';
 		conv->width = width;
-		return ;
+		return (ap);
 	}
 	if (format[*i] == '*')
 	{
-		conv->width = va_arg(ap, int);
+		conv->width = va_arg(*ap, int);
 		(*i)++;
-		return ;
+		return (ap);
 	}
+	return (ap);
 }
